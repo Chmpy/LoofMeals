2a362e684d81f164902815d214012b01fae06f7d
Roy Barneveld <roy.barneveld1@gmail.com>
2023-11-17 11:47:17 +0100

    Add todo

--- app/src/main/java/com/example/loofmeals/network/RestaurantService.kt
+++ app/src/main/java/com/example/loofmeals/network/RestaurantService.kt
@@ -8,4 +8,5 @@ interface RestaurantService {
 
     @GET
     suspend fun getRestaurantList(@Url url: String): List<Restaurant>
+    //TODO: Implement APIRestaurant data source to future proof against changes in the API and convert to domain objects from Restaurants
 }
