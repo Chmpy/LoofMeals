a4b0a3c0226a71fd8ef1951b917c6f9f66341dff
Roy Barneveld <roy.barneveld1@gmail.com>
2023-12-25 13:09:05 +0100

    Improve theming & Fix intents

--- app/src/main/java/com/example/loofmeals/ui/theme/Theme.kt
+++ app/src/main/java/com/example/loofmeals/ui/theme/Theme.kt
@@ -1,10 +1,13 @@
 package com.example.loofmeals.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 
 private val LightColorScheme = lightColorScheme(
     primary = md_theme_light_primary,
@@ -73,17 +76,24 @@ private val DarkColorScheme = darkColorScheme(
 
 @Composable
 fun LoofTheme(
-  darkTheme: Boolean = isSystemInDarkTheme(),
-  content: @Composable () -> Unit
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
 ) {
-  val colorScheme = when {
-    darkTheme -> DarkColorScheme
-    else -> LightColorScheme
-  }
 
-  MaterialTheme(
-      colorScheme = colorScheme,
-      typography = Typography,
-      content = content
-  )
+    val window = (LocalView.current.context as Activity).window
+    val themeColor =
+        if (darkTheme) DarkColorScheme.primary.toArgb() else LightColorScheme.primary.toArgb()
+    window.statusBarColor = themeColor
+    window.navigationBarColor = themeColor
+
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
 }
